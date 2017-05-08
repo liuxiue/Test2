1391ebfca3b9fa46c80c3a6ccdfad2acd3813aca
liuxiue <liuxiue18@163.com>
2017-05-08 21:37:48 +0800

    Rejected commit: Default

--- app/src/main/java/map/jiyun/com/test/Person.java
+++ app/src/main/java/map/jiyun/com/test/Person.java
@@ -5,4 +5,34 @@ package map.jiyun.com.test;
  */
 
 public class Person {
+    private int age;
+    private String name;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+
+
 }
